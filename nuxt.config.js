4603a9655944d87c386c5f8ad519568ab3e9e20c
Nick The Sick <nperez0111@gmail.com>
2017-12-25 00:23:07 -0500

    added icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,9 +13,13 @@ module.exports = {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { name: 'mobile-web-app-capable', content: 'yes' },
+            { name: 'apple-mobile-web-app-capable', content: 'yes' },
             { hid: 'description', name: 'description', content: 'Panamei Seafood Selector' }
         ],
         link: [
+            { rel: 'apple-touch-icon', href: '/Icon-60@2x.png', sizes: '120x120' },
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/Icon-60@3x.png' },
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
             { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
         ]
